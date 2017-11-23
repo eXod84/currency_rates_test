5c63677edb1d0a9db92a821077768cc1216537e9
eXod84 <exod84@gmail.com>
2017-11-23 10:48:21 +0300

    Setup configuration

--- src/redux/utils/api.js
+++ src/redux/utils/api.js
@@ -1,12 +1,9 @@
-const API_KEY = 'Z1PwJvLM43vpePHemU3VRc8yO3wCpq9T';
-const API_URL = 'https://forex.1forge.com/1.0.2';
-
 export function loadCurrencies() {
-  const url = `${API_URL}/symbols?api_key=${API_KEY}`;
+  const url = `${process.env.REACT_APP_API_URL}/symbols?api_key=${process.env.REACT_APP_API_KEY}`;
   return fetch(url).then(res => res.json());
 }
 
 export function loadRates(selectedCurrencies) {
-  const url = `${API_URL}/quotes?pairs=${selectedCurrencies.join(',')}&api_key=${API_KEY}`;
+  const url = `${process.env.REACT_APP_API_URL}/quotes?pairs=${selectedCurrencies.join(',')}&api_key=${process.env.REACT_APP_API_KEY}`;
   return fetch(url).then(res => res.json());
 }
